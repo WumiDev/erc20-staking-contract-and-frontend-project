d861069f26933547bdd769c356c7b3b22d940702
WumiDev <wumi2021@gmail.com>
2022-03-24 16:59:58 +0100

    adds stking-backend and staking-frontend repos

---
+++ staking-backend/contracts/staking-wmdt.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
